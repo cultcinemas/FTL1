dc3310374d3345eb8a092264e7a7acb6594f3504
cultcinemas <152758474+cultcinemas@users.noreply.github.com>
2024-08-31 11:20:08 +0530

    Update vars.py

--- f2lnk/vars.py
+++ f2lnk/vars.py
@@ -8,12 +8,12 @@ bisal_channel = "https://telegram.me/bisal_files"
 bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
 
 class Var(object):
-    AUTH_USERS = '7386512270:AAF_szBwnRs5neTpBAWQAOz91cKigxYh9HE' # Add id of users you want to authorize to use the bot. Separated by spaces
+    AUTH_USERS = '5685802336 5452354891 5602172369 1392184089' # Add id of users you want to authorize to use the bot. Separated by spaces
     AUTH_USERS = AUTH_USERS.split()
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '28737888'))
     API_HASH = str(getenv('API_HASH', 'aa9fc525a5e5a837256c1f0b445af447'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6975980093:AAF95hTPJZ5asfYzFqyzso3u4NUsQ_8v9j8'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7386512270:AAF_szBwnRs5neTpBAWQAOz91cKigxYh9HE'))
     name = str(getenv('name', 'bisal_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -22,13 +22,13 @@ class Var(object):
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6101337858").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "1392184089").split()]
     URL = getenv('URL', 'http://185.55.240.55:8080')
     if not URL.endswith('/'):
         URL = f'{URL}/'
     PORT = getenv('PORT', 8080)
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Tyson3290'))
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://NewMoviesRoBot:NewMoviesRoBot3290@cluster0.73vkexn.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'DATABASE_URL", "mongodb+srv://pinkybitlu:pinky7268@cluster0.dizew5m.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '@CultCinemas'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))
