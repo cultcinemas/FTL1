5d7928e009a40ead9280909fecd17856e5faa602
cultcinemas <152758474+cultcinemas@users.noreply.github.com>
2025-08-10 22:36:40 +0530

    Update vars.py

--- f2lnk/vars.py
+++ f2lnk/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     MULTI_CLIENT = True
     API_ID = int(getenv('API_ID', '28737888'))
     API_HASH = str(getenv('API_HASH', 'aa9fc525a5e5a837256c1f0b445af447'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7386512270:AAGz668oDH58cDKNvj5VJQ9sNBhpGcW3qBE'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7476704065:AAEUBhA8zwWQzOBd9Y8QwzNk-8hJHDbA6Rc'))
     name = str(getenv('name', 'bisal_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '8'))
